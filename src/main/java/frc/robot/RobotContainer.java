efde0a9ff1f2bcde14e83b0a607540a9f12d0c94
AlphaLQ <sjb2019dpo@gmail.com>
2026-01-20 08:36:20 -0800

    autos ready ✅

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -145,6 +146,8 @@ public class RobotContainer {
             new WaitCommand(0.01),
             new SequentialCommandGroup(new mtest().metertest()))
         );
+
+        SmartDashboard.putData("Auto Selector", autoChooser);
     }
 
     public Command getAutonomousCommand() {
