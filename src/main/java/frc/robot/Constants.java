641835da859746cf1615b36f76e44503c182de43
AlphaLQ <sjb2019dpo@gmail.com>
2026-02-12 21:33:10 -0800

    passing coords tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -243,10 +243,10 @@ public class Constants {
         public static final Pose3d blueDepot = new Pose3d (0.7, 6.0, 1, new Rotation3d());
 
         //blueLeftBumpCorner pose
-        public static final Pose3d blueLeftBumpCorner = new Pose3d (1.0, 6.74, 1, new Rotation3d()); //x4.03
+        public static final Pose3d blueLeftBumpCorner = new Pose3d (1.0, 7.74, 1, new Rotation3d()); //x4.03 and y was +1
 
         //blueLeftBumpCorner pose
-        public static final Pose3d blueRightBumpCorner = new Pose3d (1.0, 1.438, 1, new Rotation3d()); //x4.03
+        public static final Pose3d blueRightBumpCorner = new Pose3d (1.0, 0.0, 1, new Rotation3d()); //x4.03 and y was -1
 
         //redAimThreshold pose
         public static final Pose3d blueAimThreshold = new Pose3d (0.7, 2.080, 1, new Rotation3d());
@@ -260,10 +260,10 @@ public class Constants {
         public static final Pose3d redAimThreshold = new Pose3d (15.82, 6.0, 1, new Rotation3d());
 
         //redRightBumpCorner pose
-        public static final Pose3d redRightBumpCorner = new Pose3d (15.0, 6.74, 1, new Rotation3d()); //12.505
+        public static final Pose3d redRightBumpCorner = new Pose3d (15.0, 7.74, 1, new Rotation3d()); //12.505 and y  was + 1
 
         //redRightBumpCorner pose
-        public static final Pose3d redLeftBumpCorner = new Pose3d (15.0, 1.552, 1, new Rotation3d()); //12.505
+        public static final Pose3d redLeftBumpCorner = new Pose3d (15.0, 0.552, 1, new Rotation3d()); //12.505 and y was -1
 
         //blueDepot pose
         public static final Pose3d redDepot = new Pose3d (15.82, 2.080, 1, new Rotation3d());
