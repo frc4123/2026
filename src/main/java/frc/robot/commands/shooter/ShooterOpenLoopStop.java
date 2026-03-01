f08787fda69d2cad43c852d031a832aadde1653f
AlphaLQ <sjb2019dpo@gmail.com>
2026-02-28 17:08:08 -0800

    open loop shooter testing

---
+++ src/main/java/frc/robot/commands/shooter/ShooterOpenLoopStop.java
@@ -0,0 +1,21 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+
+public class ShooterOpenLoopStop extends Command {
+    private final Shooter shooter;
+
+    public ShooterOpenLoopStop(Shooter shooter){
+        this.shooter= shooter;
+        addRequirements(shooter);
+
+    }
+
+    @Override
+    public void execute(){
+        shooter.setShooterOpenLoopVelo(0);
+    }
+}
+
