8bc83553fdea0f7c6b33d27a25094c2ca8773942
AlphaLQ <sjb2019dpo@gmail.com>
2026-03-14 15:14:46 -0700

    accurate prediction iterations

--- src/main/java/frc/robot/utils/ShotCache.java
+++ src/main/java/frc/robot/utils/ShotCache.java
@@ -28,14 +28,14 @@ public class ShotCache {
                 swerve.getState().Pose,
                 swerve.getState().Speeds,
                 Target.getTarget(),
-                7 //was 3
+                6 //was 7, 3
             );
         } else {
             cachedShot = TurretCalculator.iterativeMovingPass(
                 swerve.getState().Pose,
                 swerve.getState().Speeds,
                 Target.getTarget(),
-                7 // was 3
+                6 // was 7, 3
             );
         }
         
