1ee5e603510a297cd5c1350918eb642b336198e4
AlphaLQ <sjb2019dpo@gmail.com>
2026-03-14 10:08:33 -0700

    cleaned aim subsystem

--- src/main/java/frc/robot/commands/turret/Aim.java
+++ src/main/java/frc/robot/commands/turret/Aim.java
@@ -2,31 +2,27 @@ package frc.robot.commands.turret;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.turret.Turret;
 
 public class Aim extends Command {
     private final Turret turret;
     private final CommandSwerveDrivetrain drivetrain;
-    private final Vision vision;
 
-    public Aim(Turret turret, CommandSwerveDrivetrain drivetrain, Vision vision) {
+    public Aim(Turret turret, CommandSwerveDrivetrain drivetrain) {
         this.turret = turret;
         this.drivetrain = drivetrain;
-        this.vision = vision;
         addRequirements(turret);
     }
 
     @Override
     public boolean isFinished() {
-        return false; // Default commands should never finish on their own
+        return false; 
     }
 
     @Override
     public void execute() {
         turret.setFieldAngle(
-            turret.targetAngle(drivetrain.getState().Pose),
-            vision.getTurretCamOffset()
+            turret.targetAngle(drivetrain.getState().Pose)
         );
     }
 }
